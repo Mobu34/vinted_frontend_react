6abfbf23ab1dfb4474482347ce100762dbe70a95
Lucas Tidas <Lucas@pc-225.home>
2020-11-16 14:30:08 +0100

    Published page initialized

--- src/components/PublishInput.js
+++ src/components/PublishInput.js
@@ -1,23 +1,46 @@
 import React from "react";
 
-const PublishInput = ({ name, text, placeholder }) => {
+const PublishInput = ({ name, text, placeholder, state, setState }) => {
+  const handleChange = (e) => {
+    setState(e.target.value);
+  };
+
   return (
     <div className="PublishInput">
       {name === "price" ? (
         <>
           <label htmlFor={name}>{text}</label>
           <div>
-            <input type="text" name={name} placeholder={placeholder} />{" "}
+            <input
+              type="text"
+              name={name}
+              placeholder={placeholder}
+              onChange={handleChange}
+            />{" "}
             <input type="checkbox" name="interested" />{" "}
             <label htmlFor="interested">
               Je suis intéressé(e) par les échanges
             </label>{" "}
           </div>
         </>
+      ) : name === "description" ? (
+        <>
+          <label htmlFor={name}>Décris ton article</label>
+          <textarea
+            name={name}
+            placeholder={placeholder}
+            onChange={handleChange}
+          ></textarea>
+        </>
       ) : (
         <>
           <label htmlFor={name}>{text}</label>
-          <input type="text" name={name} placeholder={placeholder} />
+          <input
+            type="text"
+            name={name}
+            placeholder={placeholder}
+            onChange={handleChange}
+          />
         </>
       )}
     </div>
--- src/containers/Publish.js
+++ src/containers/Publish.js
@@ -1,53 +1,135 @@
-import React from "react";
+import React, { useState } from "react";
 import Cookie from "js-cookie";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 import PublishInput from "../components/PublishInput";
 
 const Publish = () => {
-  //   console.log(Cookie.get("tokenCookie"));
+  const [file, setFile] = useState();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [brand, setBrand] = useState("");
+  const [size, setSize] = useState("");
+  const [color, setColor] = useState("");
+  const [condition, setCondition] = useState("");
+  const [location, setLocation] = useState("");
+  const [price, setPrice] = useState(0);
+
+  const token = Cookie.get("tokenCookie");
+
+  const history = useHistory();
+
+  const formData = new FormData();
+  formData.append("photo", file);
+  formData.append("product_name", title);
+  formData.append("product_description", description);
+  formData.append("brand", brand);
+  formData.append("size", size);
+  formData.append("color", color);
+  formData.append("condition", condition);
+  formData.append("city", location);
+  formData.append("product_price", price);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://vinted-react.herokuapp.com/offer/publish",
+        formData,
+        {
+          headers: { authorization: `Bearer ${token}` },
+        }
+      );
+
+      if (response.status === 200) {
+        alert(
+          "L'offre a bien été ajouté, vous allez être redirigé vers cette nouvelle offre."
+        );
+        history.push(`/offer/${response.data._id}`);
+      }
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
   return (
     <div className="Publish">
       <div className="wrapper">
         <h1 className="Publish-title">Vends ton article</h1>
-        <form className="Publish-form">
+        <form onSubmit={handleSubmit} className="Publish-form">
           <div className="Publish-file-container">
-            <input type="file" />
+            <input type="file" onChange={handleChange} />
           </div>
           <div className="Publish-title_desc-container">
             <PublishInput
               name="title"
               text="Titre"
               placeholder="ex: Chemise Sézane verte"
+              state={title}
+              setState={setTitle}
             />
-            <div className="Publish-description">
-              <label htmlFor="name">Décris ton article</label>
-              <textarea
-                name="description"
-                placeholder="ex: porté quelques fois, taille correctement"
-              ></textarea>
-            </div>
+            <PublishInput
+              name="description"
+              text="Décris ton article"
+              placeholder="ex: porté quelques fois, taille correctement"
+              state={description}
+              setState={setDescription}
+            />
           </div>
           <div className="Publish-details-container">
-            <PublishInput name="brand" text="Marque" placeholder="ex: Zara" />
+            <PublishInput
+              name="brand"
+              text="Marque"
+              placeholder="ex: Zara"
+              state={brand}
+              setState={setBrand}
+            />
             <PublishInput
               name="size"
               text="Taille"
               placeholder="ex: L / 40 / 12"
+              state={size}
+              setState={setSize}
             />
             <PublishInput
               name="color"
               text="Couleur"
               placeholder="ex: Fushia"
+              state={color}
+              setState={setColor}
             />
             <PublishInput
               name="condition"
               text="État"
               placeholder="ex: Neuf avec étiquette"
+              state={condition}
+              setState={setCondition}
             />
-            <PublishInput name="location" text="Lieu" placeholder="ex: Paris" />
+            <PublishInput
+              name="location"
+              text="Lieu"
+              placeholder="ex: Paris"
+              state={location}
+              setState={setLocation}
+            />
           </div>
           <div className="Publish-price-container">
-            <PublishInput name="price" text="Prix" placeholder="0,00 €" />
+            <PublishInput
+              name="price"
+              text="Prix"
+              placeholder="0,00 €"
+              state={price}
+              setState={setPrice}
+            />
+          </div>
+          <div>
+            <button type="submit"> Ajouter</button>
           </div>
         </form>
       </div>
